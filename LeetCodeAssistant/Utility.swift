cec438f5b838292ccd22e099e3c5e03ac40347d9
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-22 12:12:08 -0700

    add delete &#39; to replacing word
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

--- LeetCodeAssistant/Utility.swift
+++ LeetCodeAssistant/Utility.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension String {
     func replaceHtmlTag() -> String {
-        return replacingOccurrences(of: "<(￥\"[^\"]*\"|'[^']*'|[^'\">])*>", with: "", options: .regularExpression, range: self.range(of: self)).replacingOccurrences(of: "&quot;", with: "").replacingOccurrences(of: "&nbsp;", with: "").replacingOccurrences(of: "C&#39;s", with: "")
+        return replacingOccurrences(of: "<(￥\"[^\"]*\"|'[^']*'|[^'\">])*>", with: "", options: .regularExpression, range: self.range(of: self)).replacingOccurrences(of: "&quot;", with: "").replacingOccurrences(of: "&nbsp;", with: "").replacingOccurrences(of: "C&#39;s", with: "").replacingOccurrences(of: "&#39;", with: "")
     }
     
     
