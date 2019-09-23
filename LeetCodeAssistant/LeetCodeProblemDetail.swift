84da0a61c583890070816a63c3cb2c6da3b81253
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-23 10:28:58 -0700

    add LeetCodeProblemDetail protocol
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

---
+++ LeetCodeAssistant/LeetCodeProblemDetail.swift
@@ -0,0 +1,14 @@
+//
+//  LeetCodeProblemDetail.swift
+//  LeetCodeAssistant
+//
+//  Created by Ryo Togashi on 2019-09-23.
+//  Copyright © 2019 Kohei Asai. All rights reserved.
+//
+
+import Foundation
+
+
+struct LeetCodeProblemDetail {
+    
+}
