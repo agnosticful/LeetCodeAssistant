e758052f4598b0819bdb528fcd7a01b6022d6ead
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-23 21:28:39 -0700

    delete comments
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

--- LeetCodeAssistant/LeetCodeProblemDetail.swift
+++ LeetCodeAssistant/LeetCodeProblemDetail.swift
@@ -1,11 +1,3 @@
-//
-//  LeetCodeProblemDetail.swift
-//  LeetCodeAssistant
-//
-//  Created by Ryo Togashi on 2019-09-23.
-//  Copyright © 2019 Kohei Asai. All rights reserved.
-//
-
 import Foundation
 
 
