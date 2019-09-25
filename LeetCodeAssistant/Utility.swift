657bdfc002b40d1fae12fc24f0774e767cbd4e83
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-25 10:23:48 -0700

    cover all html tag
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

--- LeetCodeAssistant/Utility.swift
+++ LeetCodeAssistant/Utility.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension String {
     func removeHtmlTag() -> String {
-        return replacingOccurrences(of: "<(￥\"[^\"]*\"|'[^']*'|[^'\">])*>", with: "", options: .regularExpression, range: self.range(of: self)).replacingOccurrences(of: "&quot;", with: "").replacingOccurrences(of: "&nbsp;", with: "").replacingOccurrences(of: "C&#39;s", with: "").replacingOccurrences(of: "&#39;", with: "")
+        return replacingOccurrences(of: "<(\"[^\"]*\"|'[^']*'|[^'\">])*>", with: "", options: .regularExpression, range: self.range(of: self)).replacingOccurrences(of: "&quot;", with: "").replacingOccurrences(of: "&nbsp;", with: "").replacingOccurrences(of: "C&#39;s", with: "").replacingOccurrences(of: "&#39;", with: "")
     }
     
     
